b934c036abf414ae75efe8e6b560c499f6053f10
Yagi <c0a221392c@edu.teu.ac.jp>
2023-10-03 14:50:38 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,19 +4,30 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1600, 900
 
+delta = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0)
+}
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+    
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = [900, 400]
+
     bd_img = pg.Surface((20, 20))
     bd_img.set_colorkey((0, 0, 0))#1:黒い余白を透明にする
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
-    bd_rect = bd_img.get_rect() #1:SurfaceからRectを抽出
+    bd_rct = bd_img.get_rect() #1:SurfaceからRectを抽出
     x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
-    bd_rect.center = (x, y)
+    bd_rct.center = (x, y)
     vx, vy = +5, +5
 
     clock = pg.time.Clock()
@@ -25,11 +36,19 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        screen.blit(bg_img,[0,0])
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
-        bd_rect.move_ip(vx, vy)
-        screen.blit(bd_img, bd_rect) #1:動かす場合はimg_rect
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for key, mv in delta.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1] 
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])
+        screen.blit(kk_img, kk_rct)
+
+        bd_rct.move_ip(vx, vy)
+        screen.blit(bd_img, bd_rct) #1:動かす場合はimg_rect
         pg.display.update()
         tmr += 1
         clock.tick(50)
