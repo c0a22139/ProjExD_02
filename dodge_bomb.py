3ae69af335c828fdfddc3bc2ed99c3b0fbee6873
Yagi <c0a221392c@edu.teu.ac.jp>
2023-10-03 15:23:48 +0900

    練習5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -48,6 +48,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
+        if kk_rct.colliderect(bd_rct): #5:こうかとんと爆弾がぶつかったらreturn、colliderect()はぶつかったらTrue
+            print("GameOver")
+            return
+
         screen.blit(bg_img,[0,0])
 
         """こうかとん"""
