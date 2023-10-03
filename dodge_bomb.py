c825ba71451e16394faa1cae16101e8a2c09436e
Yagi <c0a221392c@edu.teu.ac.jp>
2023-10-03 14:18:02 +0900

    練習1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,6 @@
+import random
 import sys
 import pygame as pg
-
 
 WIDTH, HEIGHT = 1600, 900
 
@@ -11,6 +11,13 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bd_img = pg.Surface((20, 20))
+    bd_img.set_colorkey((0, 0, 0))#1:黒い余白を透明にする
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    bd_rect = bd_img.get_rect() #1:SurfaceからRectを抽出
+    x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    bd_rect.center = (x, y)
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -20,6 +27,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bd_img, bd_rect) #1:動かす場合はimg_rect
         pg.display.update()
         tmr += 1
         clock.tick(10)
